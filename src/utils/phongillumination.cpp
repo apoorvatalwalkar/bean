0a8fc803ce0b550445709a629819a5de7458b14e
eleanor-park <eleanor_park@brown.edu>
2024-12-09 23:01:55 -0500

    something

--- src/utils/phongillumination.cpp
+++ src/utils/phongillumination.cpp
@@ -45,7 +45,7 @@ glm::vec4 PhongIllumination::phongLogic(glm::vec4  position,
                                         int recur,
                                         float occlusion) {
 
-    bool isSoftShadows = false;
+    bool isSoftShadows = true;
 
     normal = glm::normalize(normal);
     glm::vec3 iRay = {incidentRay[0], incidentRay[1], incidentRay[2]};
@@ -240,26 +240,18 @@ bool PhongIllumination::checkShadow(glm::vec3 p, glm::vec3 d, float maxT){
 }
 
 float PhongIllumination::calculateShadowFactor(SceneLightData light, glm::vec4 position){
-    int sampleCount = 4;
+    int sampleCount = 16;
     float width = 15.0f;
     float height = 15.0f;
     int unshadowedCount = 0;
-    int shadowedCount = 0;
 
-    float threshold = 0.2f;
 
-    int sqrtSampleCount = 2;
-
-    for (int s = 0; s < sqrtSampleCount; s++) {
-        for (int d = 0; d < sqrtSampleCount; d++) {
+    for (int s = 0; s < sampleCount; s++) {
         float randomX = (float(std::rand()) / RAND_MAX) - 0.5f;
         float randomY = (float(std::rand()) / RAND_MAX) - 0.5f;
 
-        // float offsetX = randomX * width;
-        // float offsetY = randomY * height;
-
-        float offsetX = (s + randomX) * (width / sqrtSampleCount);
-        float offsetY = (d + randomY) * (height / sqrtSampleCount);
+        float offsetX = randomX * width;
+        float offsetY = randomY * height;
 
         glm::vec3 sampleLightPos = glm::vec3(light.pos) + glm::vec3(offsetX, offsetY, 0.0f);
         glm::vec3 sampleDir = glm::normalize(sampleLightPos - glm::vec3(position));
@@ -274,12 +266,9 @@ float PhongIllumination::calculateShadowFactor(SceneLightData light, glm::vec4 p
             unshadowedCount++;
         }
     }
-    }
 
     float shadowFactor = float(unshadowedCount) / float(sampleCount);
 
-    float bias = 0.5f;
-
-    return glm::pow(shadowFactor, bias);
+    return shadowFactor;
 }
 
